1b5d0341d81e7cc401d9ecd8c39eba864c87b52c
Bhavuk Gupta <nitingupta776@gmail.com>
2021-03-24 17:34:34 -0700

    Created an executor that gets tasks form two queues

--- app/src/main/java/com/example/threadpooldesignpattern/MainActivity.java
+++ app/src/main/java/com/example/threadpooldesignpattern/MainActivity.java
@@ -7,27 +7,59 @@ import android.os.Process;
 import android.util.Log;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
-import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 
 public class MainActivity extends AppCompatActivity {
-    private int taskCount = 0;
-    private long sysTime = 0;
-    ThreadPoolExecutor executor = null;
+    private int queue1TaskCount = 0;
+    private long queue1SysTime = 0;
+    private int queue2TaskCount = 0;
+    private long queue2SysTime = 0;
+    //ThreadPoolExecutor executor = null;
     Random rand = new Random();
+
+    private Future group1Future = null;
+    private Future group2Future = null;
+
+    private LinkedBlockingQueue<PrioritizedTask> group1Queue
+            = new LinkedBlockingQueue<>();
+    private LinkedBlockingQueue<PrioritizedTask> group2Queue
+            = new LinkedBlockingQueue<>();
+
+    private ExecutorService executor = Executors.newFixedThreadPool(2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        executor = new ThreadPoolExecutor(1, 1, 3000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadPoolExecutor.CallerRunsPolicy());
+        //executor = new ThreadPoolExecutor(1, 2, 3000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), new ThreadPoolExecutor.CallerRunsPolicy());
         new TrafficGenerator().start();
+
+        startProcessing();
     }
+
+    public void startProcessing() {
+        while (true) {
+            if (group1Future != null && group1Future.isDone()) {
+                if (group1Queue.peek() != null) {
+                    group1Future = executor.submit(group1Queue.poll());
+                }
+            }
+            if (group2Future != null && group1Future.isDone()) {
+                if (group2Queue.peek() != null) {
+                    group2Future = executor.submit(group2Queue.poll());
+                }
+            }
+        }
+    }
+
     public static int poisson(double lambda) {
-        double L = Math.exp(-lambda); double p = 1.0; int k = 0;
+        double L = Math.exp(-lambda);
+        double p = 1.0;
+        int k = 0;
         do {
             k++;
             p *= Math.random();
@@ -44,39 +76,51 @@ public class MainActivity extends AppCompatActivity {
         long startTime;
         int avgDuration;
         int priority;
+        int queue;
 
-        PrioritizedTask(int taskNum, long startTime, int avgDuration, int priority) {
+        PrioritizedTask(int taskNum, long startTime, int avgDuration, int priority, int queue) {
             this.taskNum = taskNum;
 
             this.startTime = startTime;
             this.avgDuration = avgDuration;
             this.priority = priority;
-            Log.i("ThreadPool Test", "Task No: " + taskNum + " Priority: " + priority + " Created at Time: " + startTime);
+            this.queue = queue;
+            //Log.i("ThreadPool Test", "Task No: " + taskNum + " Priority: " + priority + " Created at Time: " + startTime);
         }
 
         public void run() {
             try {
                 Process.setThreadPriority(this.priority);
-            } catch (Throwable t) { }
+            } catch (Throwable t) {
+
+            }
             int duration = (int) exponential(this.avgDuration) * 1000;
 
             //  Perform some operations proportional to the duration
-            String str = "";
-            for (int i = 0; i < duration; i++) { str += "*"; }
+            for (int i = 0; i < duration; i++) {
+            }
 
             //Compute the average system time
             if (this.priority == Process.THREAD_PRIORITY_FOREGROUND) {
-                taskCount++;
-                sysTime += (System.currentTimeMillis() - this.startTime);
+                queue1TaskCount++;
+                queue1SysTime += (System.currentTimeMillis() - this.startTime);
+            }else{
+                queue2TaskCount++;
+                queue2SysTime += (System.currentTimeMillis() - this.startTime);
             }
-            Log.i("ThreadPool Test", "Task No: " + taskNum + " Priority: " + priority + " Completed at:: " +System.currentTimeMillis());
+            //Log.i("ThreadPool Test", "Task No: " + taskNum + " Priority: " + priority + " Completed at:: " +System.currentTimeMillis()+ " Queue #: " +queue);
             if (this.taskNum % 500 == 0) {
-                Log.i("ThreadPool", "High Priority Task Count = " + taskCount + " sysTime = " + sysTime + " Average Sys Time = " + (sysTime / taskCount));
-                taskCount = 0;  sysTime = 0;
+                Log.i("ThreadPool", "Queue 1 Task Count = " + queue1TaskCount + " sysTime = " + queue1SysTime + " Average Sys Time = " + (queue1SysTime / queue1TaskCount));
+                queue1TaskCount = 0;
+                queue1SysTime = 0;
+                Log.i("ThreadPool", "Queue 2 Task Count = " + queue2TaskCount + " sysTime = " + queue2SysTime + " Average Sys Time = " + (queue2SysTime / queue2TaskCount));
+                queue2TaskCount = 0;
+                queue2SysTime = 0;
             }
         }
     }
     class TrafficGenerator extends Thread {
+        // get the thread pool manager instance
         public void run() {
             for (int i = 1; i <=5000; i++) {
                 int delay = poisson(2);
@@ -87,9 +131,13 @@ public class MainActivity extends AppCompatActivity {
                 }
                 int j = rand.nextInt(2);
                 if (j % 2 == 0) {
-                    executor.submit(new PrioritizedTask(i, System.currentTimeMillis(),20, Process.THREAD_PRIORITY_FOREGROUND));
+                    // This one contains Thread Priority: -2
+                    group1Future = executor.submit(new PrioritizedTask(i, System.currentTimeMillis(),20, Process.THREAD_PRIORITY_FOREGROUND,2));
+                    group1Queue.add(new PrioritizedTask(i, System.currentTimeMillis(),20, Process.THREAD_PRIORITY_FOREGROUND,2));
                 } else {
-                    executor.submit(new PrioritizedTask(i, System.currentTimeMillis(),20, THREAD_PRIORITY_BACKGROUND));
+                    // This one contains Thread Priority: 10
+                    group2Future = executor.submit(new PrioritizedTask(i, System.currentTimeMillis(),20, Process.THREAD_PRIORITY_FOREGROUND,2));
+                    group2Queue.add(new PrioritizedTask(i, System.currentTimeMillis(),20, Process.THREAD_PRIORITY_BACKGROUND,1));
                 }
             }
         }
